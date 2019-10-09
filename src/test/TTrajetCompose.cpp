0d8d0ea4f812b7fcc957294e7bfa397da54ecedd
Serrania Aleryc <asarrania@if501-219-01.insa-lyon.fr>
2019-10-09 21:30:39 +0200

    Add last test classes

---
+++ src/test/TTrajetCompose.cpp
@@ -0,0 +1,86 @@
+/*************************************************************************
+                            TTrajetCompose  -  description
+                              --------------------
+   début                : 09/10/2019
+   copyright            : (C) 2019 par Aleryc SERRANIA
+*************************************************************************/
+
+//---------- Réalisation du module <TTrajetCompose> (fichier TTrajetCompose.cpp) -----
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "../../includes/test/TTrajetCompose.h"
+#include "../../includes/TrajetCompose.h"
+#include "../../includes/TrajetSimple.h"
+#include "../../includes/CollectionTrajets.h"
+
+///////////////////////////////////////////////////////////////////  PRIVE
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//---------------------------------------------------- Variables statiques
+
+//------------------------------------------------------ Fonctions privées
+//static type nom ( liste de paramètres )
+// Mode d'emploi :
+//
+// Contrat :
+//
+// Algorithme :
+//
+//{
+//} //----- fin de nom
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+
+//---------------------------------------------------- Fonctions publiques
+void testUnSeulTrajet()
+{
+    Trajet * t1 = new TrajetSimple("A", "B", AVION);
+    CollectionTrajets ct;
+    ct.AjouterTrajet(t1);
+    TrajetCompose tc (ct);
+    cout << tc.VilleDepart() << endl;
+    cout << tc.VilleArrivee() << endl;
+    tc.Afficher();
+    cout << endl;
+    // Devrait afficher :
+    // A
+    // B
+    // De A à B en Avion
+}
+
+void testPlusieursTrajets()
+{
+    Trajet * t1 = new TrajetSimple("A", "B", AVION);
+    Trajet * t2 = new TrajetSimple("B", "E", TRAIN);
+    Trajet * t3 = new TrajetSimple("E", "A", BATEAU);
+    CollectionTrajets ct;
+    ct.AjouterTrajet(t1);
+    ct.AjouterTrajet(t2);
+    ct.AjouterTrajet(t3);
+
+    TrajetCompose tc (ct);
+    cout << tc.VilleDepart() << endl;
+    cout << tc.VilleArrivee() << endl;
+    tc.Afficher();
+    cout << endl;
+    // Devrait afficher :
+    // A
+    // A
+    // De A à B en Avion - De B à E en Train - De E à A en Bateau
+}
+
+int main ()
+{
+    testUnSeulTrajet();
+    testPlusieursTrajets();
+    return 0;
+}
---
+++ src/test/TTrajetSimple.cpp
@@ -0,0 +1,87 @@
+/*************************************************************************
+                            TTrajetSimple  -  description
+                              --------------------
+   début                : 09/10/2019
+   copyright            : (C) 2019 par Aleryc SERRANIA
+*************************************************************************/
+
+//---------- Réalisation du module <TTrajetSimple> (fichier TTrajetSimple.cpp) -----
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "../../includes/test/TTrajetSimple.h"
+#include "../../includes/TrajetSimple.h"
+
+///////////////////////////////////////////////////////////////////  PRIVE
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//---------------------------------------------------- Variables statiques
+
+//------------------------------------------------------ Fonctions privées
+//static type nom ( liste de paramètres )
+// Mode d'emploi :
+//
+// Contrat :
+//
+// Algorithme :
+//
+//{
+//} //----- fin de nom
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+
+//---------------------------------------------------- Fonctions publiques
+void testConstructeur()
+{
+    TrajetSimple t("A", "B", AUTO);
+    t.Afficher();
+    cout << endl;
+    cout << t.VilleDepart() << endl;
+    cout << t.VilleArrivee() << endl;
+    // Devrait afficher :
+    // De A à B en Auto
+    // A
+    // B
+}
+
+void testConstructeur2()
+{
+    TrajetSimple t("ABCDEFGH", "IJKLMNOP", BATEAU);
+    t.Afficher();
+    cout << endl;
+    cout << t.VilleDepart() << endl;
+    cout << t.VilleArrivee() << endl;
+    // Devrait afficher :
+    // De ABCDEFGH à IJKLMNOP en Bateau
+    // ABCDEFGH
+    // IJKLMNOP
+}
+
+void testConstructeurVillesCopiees()
+{
+    const char villeDepart[] = "A";
+    const char villeArrivee[] = "B";
+    TrajetSimple t(villeDepart, villeArrivee, BATEAU);
+    cout << (t.VilleDepart() == villeDepart) << endl;
+    cout << (t.VilleArrivee() == villeArrivee) << endl;
+    // Devrait afficher :
+    // 0
+    // 0
+
+}
+
+int main()
+{
+    testConstructeur();
+    testConstructeur2();
+    testConstructeurVillesCopiees();
+    return 0;
+}
